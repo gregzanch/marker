65b1bb6da76401f971d734b31e1011099aa17b44
Greg Zanchelli <zanchelli.greg@gmail.com>
2025-01-03 06:21:53 +0530

    new logo (#20)

--- app/src/components/Home.svelte
+++ app/src/components/Home.svelte
@@ -5,6 +5,30 @@
 </script>
 
 <Layout>
-  <Logo />
+  <div class="vertical">
+  <Logo animate={true} />
   <a href="/create"><button class="primary">Create Room</button></a>
+</div>
 </Layout>
+
+<style>
+    a {
+      width: 100%;
+      border-bottom-left-radius: 8px;
+      border-bottom-right-radius: 8px;
+    }
+    button {
+      width: 100%;
+      border-top-left-radius: 0px;
+      border-top-right-radius: 0px;
+      border-width: 1px;
+      border-top-width: 0px;
+      border-color: var(--mono-darkest);
+    }
+    .vertical {
+    display: flex;
+    flex-direction: column;
+    justify-content: end;
+    align-items: center;
+  }
+</style>
