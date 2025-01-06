7ae31dc3a467cb12036c2f30290e0f772f0cf74b
Greg Zanchelli <zanchelli.greg@gmail.com>
2025-01-06 08:12:47 +0530

    Deployment (#21)
    * Add `app/dist` to repo for ease of deployment
    
    * Fix dockerfile
    
    * remove dist
    
    * add dev flag
    
    * add fly congif

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 if [ -f ./server/marker ]; then
+  export DEV="Anton"
   cd server && ./marker
 else
   echo "ERROR: No executable found."
--- server/go.mod
+++ server/go.mod
@@ -1,6 +1,6 @@
 module gregzan.ch/marker
 
-go 1.23.3
+go 1.23
 
 require (
 	github.com/gorilla/mux v1.8.1
